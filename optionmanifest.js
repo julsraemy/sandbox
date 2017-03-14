7f391c9bf5c0a9879f5f1c42397fc99e179f3dea
Julien A. Raemy <julien.raemy@gmail.com>
2017-03-14 08:25:32 +0100

    Add HTML mimic pages and update JS

--- optionmanifest.js
+++ optionmanifest.js
@@ -1,8 +1,21 @@
 // Select a IIIF manifest from <select> to populate text value and the IIIF logo href
 function run() {
-	var link = document.getElementById("manifestSelect").value;
+	var link = document.getElementById("manifestSelect").value1;
     	document.getElementById("manifest").value = link;
    		document.getElementById("dragndrop").href = link;
     return false;
     			}
-    			
+
+function run() {
+	var link = document.getElementById("manifestSelect").value2;
+    	document.getElementById("manifest").value = link;
+   		document.getElementByID("uvIcon").href = link;
+    return false;
+    			}
+
+function run() {
+	var link = document.getElementById("manifestSelect").value3;
+    	document.getElementById("manifest").value = link;
+   		document.getElementByID("miradorIcon").href = link;
+    return false;
+    			}      			
